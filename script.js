49b3e72cc99ace2947a0282c63b93b5cfcf4c3a8
fwijaya918 <fwijaya918@gmail.com>
2023-05-08 18:05:15 +0700

    tolong rotate in ya

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ loader.load(
         base.castShadow = true;
         base.position.x = 0;
         base.position.y = 0;
-        base.position.z = 60;
+        base.position.z = 65;
         base.scale.x = 10;
         base.scale.y = 10;
         base.scale.z = 10;
@@ -81,13 +81,103 @@ loader.load(
         starship.receiveShadow = true;
         starship.castShadow = true;
         starship.position.x = 0;
-        starship.position.y = 10;
-        starship.position.z = 50;
+        starship.position.y = 0;
+        starship.position.z = 0;
         starship.scale.x = 6;
         starship.scale.y = 6;
         starship.scale.z = 6;
         console.log(starship)
         scene.add(starship)
+    }
+)
+
+let gedung_2;
+loader.load(
+    'models/gedung_2/scene.gltf',
+    function (gltf) {
+        gedung_2 = gltf.scene;
+        gedung_2.receiveShadow = true;
+        gedung_2.castShadow = true;
+        gedung_2.position.x = -80;
+        gedung_2.position.y = -3;
+        gedung_2.position.z = 40;
+        gedung_2.scale.x = 0.01;
+        gedung_2.scale.y = 0.01;
+        gedung_2.scale.z = 0.01;
+        console.log(gedung_2)
+        scene.add(gedung_2)
+    }
+)
+
+let building_felix;
+loader.load(
+    'models/building_felix/scene.gltf',
+    function (gltf) {
+        building_felix = gltf.scene;
+        building_felix.receiveShadow = true;
+        building_felix.castShadow = true;
+        building_felix.position.x = -50;
+        building_felix.position.y = 0;
+        building_felix.position.z = 100;
+        building_felix.scale.x = 0.1;
+        building_felix.scale.y = 0.1;
+        building_felix.scale.z = 0.1;
+        console.log(building_felix)
+        scene.add(building_felix)
+    }
+)
+let pesawat;
+loader.load(
+    'models/pesawat/scene.gltf',
+    function (gltf) {
+        pesawat = gltf.scene;
+        pesawat.receiveShadow = true;
+        pesawat.castShadow = true;
+        pesawat.position.x = 15;
+        pesawat.position.y = 50;
+        pesawat.position.z = 100;
+        pesawat.scale.x = 1;
+        pesawat.scale.y = 1;
+        pesawat.scale.z = 1;
+        pesawat.rotation.y += 5;
+        pesawat.rotation.z += 1;
+        console.log(pesawat)
+        scene.add(pesawat)
+    }
+)
+let rumah_kotak;
+loader.load(
+    'models/rumah_kotak/scene.gltf',
+    function (gltf) {
+        rumah_kotak = gltf.scene;
+        rumah_kotak.receiveShadow = true;
+        rumah_kotak.castShadow = true;
+        rumah_kotak.position.x = -25;
+        rumah_kotak.position.y = 11;
+        rumah_kotak.position.z = -25;
+        rumah_kotak.scale.x = 5;
+        rumah_kotak.scale.y = 5;
+        rumah_kotak.scale.z = 5;
+        rumah_kotak.rotation.y += 4;
+        console.log(rumah_kotak)
+        scene.add(rumah_kotak)
+    }
+)
+let gedung_bulet;
+loader.load(
+    'models/gedung_bulet/scene.gltf',
+    function (gltf) {
+        gedung_bulet = gltf.scene;
+        gedung_bulet.receiveShadow = true;
+        gedung_bulet.castShadow = true;
+        gedung_bulet.position.x = 20;
+        gedung_bulet.position.y = -5;
+        gedung_bulet.position.z = 100;
+        gedung_bulet.scale.x = 1;
+        gedung_bulet.scale.y = 1;
+        gedung_bulet.scale.z = 1;
+        console.log(gedung_bulet)
+        scene.add(gedung_bulet)
     }
 )
 
@@ -112,22 +202,39 @@ function processKeyboard(delta) {
     let speed = 100;
     let actualSpeed = speed * delta;
     if (keyboard["w"]) {
-        controls.moveForward(actualSpeed);
+        if(cam.position.z>-250 && cam.position.z<250  && cam.position.x > -200 && cam.position.x < 200){
+            controls.moveForward(actualSpeed);
+        }
     }
     if (keyboard["a"]) {
-        controls.moveRight(-actualSpeed);
+        if (cam.position.x > -200) {
+            controls.moveRight(-actualSpeed);
+        }
+
     }
     if (keyboard["s"]) {
-        controls.moveForward(-actualSpeed);
+        if(cam.position.z>-250 && cam.position.z<250 && cam.position.x > -200 && cam.position.x < 200){
+            controls.moveForward(-actualSpeed);
+        }
+        
     }
     if (keyboard["d"]) {
-        controls.moveRight(actualSpeed);
+        if (cam.position.x < 200) {
+            controls.moveRight(actualSpeed);
+        }
+
     }
     if (keyboard["Control"]) {
-        cam.translateY(-actualSpeed);
+        // console.log(cam.position.y)
+        if (cam.position.y > 0) {
+            cam.translateY(-actualSpeed);
+        }
     }
     if (keyboard[" "]) {
-        cam.translateY(actualSpeed);
+        if (cam.position.y < 120) {
+            cam.translateY(actualSpeed);
+        }
+
     }
 }
 
